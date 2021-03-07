cc67190b27636011bc685f431e888be92481dcb8
Pavlos Vinieratos <pvinis@gmail.com>
2021-03-07 14:35:23 +0000

    more work

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 
 ## How to use it right now!
 - Click the `Use this template` button at the top of this repo.
+- Click `Create Token`.
+- On `Edit zone DNS`, click `Use template`.
+- Edit the token name on top, maybe something like `github-dns-records`
+- Edit the `Zone Resources` to include a specific zone, or zones of an account, or all zones.
+- Click `Continue to summary`.
+- Verify things looks right, and click `Create Token`.
+
+
+## Proudly used by:
--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-const cloudflare = require("cloudflare-cli")
-const hjson = require("hjson")
+const { exit } = require('process')
 
+const main = async () => {
+	require('dotenv').config()
+
+	const HJSON = require("hjson")
+	const fs = require("fs")
+	const Cloudflare = require("cloudflare")
+	const cf = new Cloudflare({
+		token: myToken
+	})
+
+	const rawText = fs.readFileSync("./DNS-RECORDS.hjson").toString()
+	const config = HJSON.parse(rawText)
+
+
+	// find the right zone
+	const zones = await cf.zones.browse()
+	const theZones = zones.result.filter(zone => zone.name === config.zone).map(zone => zone.id)
+	if (theZones.length === 0) {
+		console.log(`No zones found with name: ${config.zone}`)
+		console.log("Make sure you have it right in DNS-RECORDS.hjson")
+		exit(-1)
+	}
+
+	const zoneId = theZones[0]
+
+	console.log(JSON.stringify(zoneId))
