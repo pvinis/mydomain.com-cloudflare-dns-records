8bf5ed5597dd8591f5f8458e23a340fc846722ad
Pavlos Vinieratos <pvinis@gmail.com>
2021-03-07 15:17:10 +0000

    more

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const main = async () => {
 	const config = HJSON.parse(rawText)
 
 
-	// find the right zone
+	// Find the right zone
 	const zones = await cf.zones.browse()
 	const theZones = zones.result.filter(zone => zone.name === config.zone).map(zone => zone.id)
 	if (theZones.length === 0) {
@@ -25,4 +25,103 @@ const main = async () => {
 
 	const zoneId = theZones[0]
 
-	console.log(JSON.stringify(zoneId))
+
+	// Check which records need to be deleted, kept, or added
+	const currentRecords = await cf.dnsRecords.browse(zoneId)
+	console.log(JSON.stringify(currentRecords))
+
+	const toBeDeleted = []
+	const toBeKept = []
+	const toBeAdded = config.records
+
+	const niceRecordName = (rec) => {
+		const removeZone = rec.name.replace(rec.zone_name, "")
+		if (removeZone === "") return "@"
+		return removeZone.slice(0, -1)
+	}
+
+	const sameRecord = (rec) => {
+		const sameRecordIdx = toBeAdded.findIndex(possiblySameRec => {
+			if (rec.type !== possiblySameRec.type) return false
+
+			const niceName = niceRecordName(rec)
+			if (niceName !== possiblySameRec.name) return false
+
+			switch (rec.type) {
+				case "A":
+					if (rec.content !== possiblySameRec.ipv4) return false
+
+
+				case "TXT":
+					if (rec.content !== possiblySameRec.content) return false
+
+				default: break
+			}
+
+			return true
+		})
+
+		if (sameRecordIdx === -1) {
+			return true
+		}
+
+		toBeAdded.splice(sameRecordIdx, 1)
+		return false
+	}
+
+	currentRecords.forEach(rec => {
+		if (sameRecord(rec)) {
+			toBeDeleted.push(rec)
+		} else {
+			toBeKept.push(rec)
+		}
+	})
+
+	console.log("Records that will be deleted:")
+	toBeDeleted.forEach(rec => {
+		console.log(`- ${rec.type} ${rec.name} ${rec.content}`)
+	})
+
+	toBeDeleted.forEach(rec => {
+		cf.dnsRecords.del(zoneId, rec.id)
+	})
+
+	console.log("Records that will be kept:")
+	toBeKept.forEach(rec => {
+		console.log(`- ${rec.type} ${rec.name} ${rec.content}`)
+	})
+
+	console.log("Records that will be added:")
+	toBeAdded.forEach(rec => {
+		console.log(`- ${rec.type} ${rec.name} ${rec.content}`)
+	})
+
+	toBeAdded.forEach(rec => {
+
+		switch (rec.type) {
+			case "A":
+				cf.dnsRecords.add(zoneId, {
+					type: rec.type,
+					name: rec.name,
+					content: rec.ipv4,
+					proxied: rec.proxied ?? true,
+				})
+				break
+
+
+			case "TXT":
+				cf.dnsRecords.add(zoneId, {
+					type: rec.type,
+					name: rec.name,
+					content: rec.content,
+				})
+				break
+
+			default: break
+		}
+	})
+
+
+
+}
+main()
